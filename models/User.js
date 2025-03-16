a81a00457f3d1286356d8835c6ee78a0cdf5fa7c
MrNamra <namraramsha@yahoo.com>
2025-03-16 11:05:49 +0530

    server issue fic

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const bycrypt = require('bcrypt');
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
